517d9dfb53dc3bd95664c896246d094f7e3d014a
CleitonCorrea <cleitoncorrea2013@hotmail.com>
2025-03-26 08:57:30 -0300

    Initial commit

---
+++ src/main/java/com/cleitoncorrea/api_rest_whit_springboot/ApiRestWhitSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cleitoncorrea.api_rest_whit_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWhitSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestWhitSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleitoncorrea/api_rest_whit_springboot/ApiRestWhitSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitoncorrea.api_rest_whit_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWhitSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
